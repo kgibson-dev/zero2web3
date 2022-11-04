785b096232d7254d7d526b99546f6bc3e6490784
G1bb074 <keithwgibson74@gmail.com>
2022-11-04 15:05:01 +0000

    Updated content

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const viewMore = document.getElementById('view-more')
 const readMoreArticles = document.getElementById('read-more-articles')
 
-
-
 viewMore.addEventListener('click', function(){
     if (viewMore.textContent.includes("more")) {
 		viewMore.textContent = "view less"
